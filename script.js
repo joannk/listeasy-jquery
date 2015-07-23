71b1b1b134635b491cc44d819aa0b53ee1e1991a
Joann Kim <jsk031@gmail.com>
2015-07-23 17:11:53 -0400

    Completed

--- script.js
+++ script.js
@@ -4,13 +4,26 @@ var template = function(text) {
 
 var main = function() {
   $('form').submit(function() {
-      
-    
-    
-    return false;  
-  }
-  
-  
+    var text = $("#todo").val(),
+        formatted_text = template(text);
+
+  $(".list").append(formatted_text);
+
+  $("#todo").val("");
+
+    return false;
+  });
+
+
+  $(".list").on('click', '.glyphicon-star', function(){
+    $(this).toggleClass("active");
+  })
+
+  $(".list").on('click', '.glyphicon-remove', function(){
+    $(this).closest('p').remove();
+  });
+
+
 };
 
 $(document).ready(main);
